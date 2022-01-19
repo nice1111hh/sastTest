0deeef5296334cb673c4e03b986d5340afd47869
Mukesh kumar <mukeshit91@gmail.com>
2022-01-19 11:12:44 +0000

    gitsecret_check_severity.py edited online with Bitbucket

--- gitsecret_check_severity.py
+++ gitsecret_check_severity.py
@@ -18,7 +18,7 @@ for i in range(len(test_txt['results'])):
  count+=1
  #print (test_txt['results'][i]['found_by'])
 
- if ((test_txt['results'][i]['severity']) == 'High' or (test_txt['results'][i]['severity']) == 'Medium') and (test_txt['results'][i]['found_by']) == 19:
+ if ((test_txt['results'][i]['severity']) == 'High' or (test_txt['results'][i]['severity']) == 'Medium') and (test_txt['results'][i]['found_by']) == [19]:
     count+=1
     print('severity is High/Medium so pipeline terminated')
     print(count)
@@ -29,4 +29,4 @@ for i in range(len(test_txt['results'])):
     continue
 
 print(count)
-exit(1)
+#exit(1)
