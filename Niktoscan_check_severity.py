ce9a9306f5837ffebc0f7f3c70c1a3827054f1c3
Mukesh kumar <mukeshit91@gmail.com>
2022-01-21 06:06:17 +0000

    Niktoscan_check_severity.py edited online with Bitbucket

--- Niktoscan_check_severity.py
+++ Niktoscan_check_severity.py
@@ -2,8 +2,8 @@ import json
 import requests
 import sys
 
-url = 'http://3.135.238.207:8080/api/v2/findings?limit=1000'
-
+#url = 'http://3.135.238.207:8080/api/v2/findings?limit=1000'
+url = 'http://3.135.238.207:8080/api/v2/findings/?tags=&test__tags=BUILD_ID'
 headers = {'content-type': 'application/json',
            'Authorization': 'Token 04a3f27e413800d03838d1d5ac9c5dcdb91e672b'}
 r = requests.get(url, headers=headers, verify=True)# set verify to False if ssl cert is self-signed
@@ -15,14 +15,15 @@ for i in range(len(test_txt['results'])):
  count+=1
  #print (test_txt['results'][i]['found_by'])
 
- if (test_txt['results'][i]['severity']) == 'High' or (test_txt['results'][i]['severity']) == 'Medium' and (test_txt['results'][i]['found_by']) == 72:
-
-    print('severity is High/Medium so pipeline terminated')
-    # print(count)
+ if ((test_txt['results'][i]['severity']) == 'High' or (test_txt['results'][i]['severity']) == 'Medium') and (test_txt['results'][i]['found_by']) == [72]:
+    
+    print('severity is High/Medium found in Nikto scan so pipeline terminated')
+    #print(count)
     exit(1)
 
  else:
-    #print('there are no high/medium found so pipeline continue' )
+    print('there are no high/medium found so pipeline continue' )
     continue
 
 print(count)
+#exit(1)
