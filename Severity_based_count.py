aa86f2b9c83d7b300f8951d0447764e3e51912f0
Mukesh kumar <mukeshit91@gmail.com>
2022-01-25 03:07:05 +0000

    Severity_based_count.py edited online with Bitbucket

--- Severity_based_count.py
+++ Severity_based_count.py
@@ -4,7 +4,7 @@ import requests
 
 import sys
 
-url = 'http://3.135.238.207:8080/api/v2/findings/?tags=&test__tags=210'
+url = 'http://3.135.238.207:8080/api/v2/findings/?tags=&test__tags=BUILD_ID'
 headers = {'content-type': 'application/json',
 'Authorization': 'Token 04a3f27e413800d03838d1d5ac9c5dcdb91e672b'}
 r = requests.get(url, headers=headers, verify=True) # set verify to False if ssl cert is self-signed
