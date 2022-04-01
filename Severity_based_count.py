c1a7b01de2361cf96782b6632a50f7331830602b
Mukesh kumar <mukeshit91@gmail.com>
2022-04-01 14:12:47 +0000

    Severity_based_count.py edited online with Bitbucket

--- Severity_based_count.py
+++ Severity_based_count.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import sys
+import smtplib
 url = 'http://3.135.238.207:8080/api/v2/findings/?tags=&test__tags=BUILD_ID'
 headers = {'content-type': 'application/json',
 'Authorization': 'Token 04a3f27e413800d03838d1d5ac9c5dcdb91e672b'}
@@ -34,7 +35,16 @@ print('High Count is: ', count_high)
 print('Medium Count is: ', count_medium)
 
 if count_high > 2:
-    
+   
+    s = smtplib.SMTP()
+    s.connect('email-smtp.us-east-1.amazonaws.com', 587)
+    s.starttls()
+    s.login('AKIAWVM3U6DD6PPTNVVU', 'BAnvfly44m17RnmWbQvT2bq0BCES+tQrjZrGf/Pz5ITI')
+
+    msg = 'From: mukeshit100@gmail.com\nTo: mukeshit91@gmail.com\nSubject: Test email\n\n Report has High severity please fix as soon as possible'
+    s.sendmail('mukeshit100@gmail.com','mukeshit91@gmail.com', msg)
+
+   
     print("more than 2  high severity found so terminated pipeline")
     exit(1)
 elif count_medium >2:
